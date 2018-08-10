7340a987bbd59fea0521bb0719465d734ca17314
Tim <tim@tmshkr.com>
2018-08-10 04:11:49 +0000

    Initial commit

---
+++ breathing.js
@@ -0,0 +1,39 @@
+var circle = document.getElementById("circle")
+var text = document.getElementById("text")
+var textDisplay = true
+var inhale = true
+
+
+function breathe() {
+
+      if (inhale){
+        circle.style.transform = "scale(4)"
+        text.textContent = "inhale"
+        window.setTimeout(function() {
+          text.textContent = "hold"
+        }, 4000)
+        inhale = false
+      }
+      else if (!inhale){
+        circle.style.transform = "scale(1)"
+        text.textContent = "exhale"
+        window.setTimeout(function() {
+          text.textContent = "hold"
+        }, 4000)
+        inhale = true
+      }
+}
+
+function toggleText() {
+  if (textDisplay){
+    text.style.color = "transparent"
+    textDisplay = false
+  }
+  else if (!textDisplay){
+    text.style.color = "white"
+    textDisplay = true
+  }
+}
+
+text.onclick = toggleText
+setInterval(breathe, 8000)
